25190bbe9965bade7d21135533dbfc7752b6551e
cullyfung <cullyfung@gmail.com>
2023-01-28 19:11:15 +0800

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './components/**/*.{js,vue,ts}',
     './layouts/**/*.vue',
@@ -9,7 +10,22 @@ module.exports = {
     './app.vue'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        dim: {
+          50: '#5F99F7',
+          100: '#5F99F7',
+          200: '#38444d',
+          300: '#202e3a',
+          400: '#253341',
+          500: '#5F99F7',
+          600: '#5F99F7',
+          700: '#192734',
+          800: '#162d40',
+          900: '#15202b'
+        }
+      }
+    }
   },
   plugins: []
 }
