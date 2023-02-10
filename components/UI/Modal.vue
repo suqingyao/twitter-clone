ca6d8995ed57b99f43c57621f6a86da5bc2dcb06
cullyfung <cullyfung@gmail.com>
2023-02-10 23:59:00 +0800

    feat: add modal

---
+++ components/UI/Modal.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import {
+  Dialog,
+  DialogDescription,
+  DialogPanel,
+  DialogTitle
+} from '@headlessui/vue'
+
+const emits = defineEmits<{
+  (event: 'on-close'): void
+  (event: 'update:modelValue', value: boolean): void
+}>()
+
+const { modelValue } = defineProps<{
+  modelValue: boolean
+}>()
+
+const onClose = () => {
+  emits('on-close')
+}
+</script>
+
+<template>
+  <Dialog :open="modelValue" @close="onClose">
+    <DialogPanel>
+      <DialogTitle><slot name="title" /></DialogTitle>
+      <DialogDescription>
+        <slot name="description" />
+      </DialogDescription>
+      <slot />
+    </DialogPanel>
+  </Dialog>
+</template>
