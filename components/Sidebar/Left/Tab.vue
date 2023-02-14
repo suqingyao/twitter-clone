cd95c916a9ee0aa40fe084d9807986f5a69a04c3
cullyfung <cullyfung@gmail.com>
2023-02-15 00:45:16 +0800

    chore: dark mode style update

--- components/Sidebar/Left/Tab.vue
+++ components/Sidebar/Left/Tab.vue
@@ -9,10 +9,10 @@ const textCls = computed(() => (active ? 'font-semibold' : 'font-normal'))
 
 <template>
   <NuxtLink
-    to="#"
+    :to="to"
     class="flex w-min items-center rounded-full p-3 text-black hover:bg-gray-200 dark:text-white dark:hover:bg-dim-200 default-transition"
   >
-    <div class="text-dark text-xl">
+    <div class="text-dark text-xl text-black dark:text-white">
       <slot name="icon" />
     </div>
     <div class="ml-4 hidden text-xl xl:block" :class="textCls">
